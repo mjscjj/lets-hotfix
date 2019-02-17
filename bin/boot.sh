2e5599370b0d97c69a9e1bc509abc8da0a8bfaa0
liuzhengyang <liuzhengyang75@gmail.com>
2019-02-17 19:36:51 +0800

    optimize boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 eurekaServer=$1
-localUrl=$2
 hostname=$(hostname -f)
-[ $# -eq 0 ] && { echo "Usage: $0 <eureka-server> <local-url>"; \
-echo "Example: $0 localhost:8761 http://localhost:18086"; exit 1; }
+[ $# -eq 0 ] && { echo "Usage: $0 <eureka-server>"; \
+echo "Example: $0 localhost:8761 "; exit 1; }
 
-git clone https://github.com/liuzhengyang/lets-hotfix
-cd lets-hotfix
+REPOSRC=https://github.com/liuzhengyang/lets-hotfix
+LOCALREPO=lets-hotfix
+
+# We do it this way so that we can abstract if from just git later on
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+    cd $LOCALREPO
+else
+    cd $LOCALREPO
+    git pull
+fi
+
 hotfixHome=`pwd`
 echo $hotfixHome
 ./mvnw clean package -pl agent
@@ -15,5 +27,4 @@ echo $hotfixHome
 agentPath=$hotfixHome/agent/target/agent-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 ./mvnw spring-boot:run -Dserver.port=18086 -Dagent\
-.path=$agentPath -Deureka.client.service-url.defaultZone=http://$eurekaServer/eureka/ -Deureka\
-.instance.home-page-url=$localUrl -Deureka.instance.hostname=${hostname} -pl web
+.path=$agentPath -Deureka.client.service-url.defaultZone=http://$eurekaServer/eureka/ -pl web
