59dbc400ecdb7ef78c2dd749adbab08bff682b7e
刘正阳 <liuzhengyang@users.noreply.github.com>
2019-03-01 23:51:32 +0800

    [WIP]use async programming model (#20)
    * use async programming model
    replace retrofit with spring web client
    
    * remove retrofit and okhttp
    
    * make all api returns mono

--- web/src/main/java/com/github/lzy/hotfix/controller/MainController.java
+++ web/src/main/java/com/github/lzy/hotfix/controller/MainController.java
@@ -2,7 +2,6 @@ package com.github.lzy.hotfix.controller;
 
 import static java.util.stream.Collectors.toList;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Collections;
@@ -15,7 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,17 +23,13 @@ import org.springframework.web.multipart.MultipartFile;
 import com.github.lzy.hotfix.model.HotfixResult;
 import com.github.lzy.hotfix.model.JvmProcess;
 import com.github.lzy.hotfix.model.Result;
-import com.github.lzy.hotfix.proxy.AgentProxyClient;
+import com.github.lzy.hotfix.proxy.AgentWebClient;
 import com.github.lzy.hotfix.service.HotfixService;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import retrofit2.Retrofit;
-import retrofit2.converter.jackson.JacksonConverterFactory;
+import reactor.core.publisher.Mono;
 
 /**
  * @author liuzhengyang
@@ -50,9 +46,11 @@ public class MainController {
     @Resource
     private HotfixService hotfixService;
 
-    @RequestMapping("/")
+    @Resource
+    private AgentWebClient agentWebClient;
+
+    @GetMapping("/")
     public String main(Model model) throws UnknownHostException {
-        model.addAttribute("processList", hotfixService.getProcessList());
         model.addAttribute("hostname", InetAddress.getLocalHost().getHostName());
         Application application = eurekaClient.getApplication(APPLICATION_NAME);
         List<InstanceInfo> instances = Optional.ofNullable(application)
@@ -61,50 +59,34 @@ public class MainController {
         return "main";
     }
 
-    @RequestMapping("/processList")
+    @GetMapping("/processList")
     @ResponseBody
-    public Result<List<JvmProcess>> processList(@RequestParam(value = "proxyServer",
-            required = false) String proxyServer) throws IOException {
-        if (proxyServer != null && !proxyServer.isEmpty()) {
-            return getProxyClient(proxyServer).getJvmProcess().execute().body();
-        }
-        return Result.success(hotfixService.getProcessList());
+    public Mono<Result<List<JvmProcess>>> processFlux(@RequestParam(value = "proxyServer",
+            required = false) String proxyServer) {
+        return Mono.justOrEmpty(proxyServer)
+                .flatMap(proxy -> agentWebClient.getJvmProcess(proxy))
+                .switchIfEmpty(Mono.fromCallable(() -> Result.success(hotfixService.getProcessList())));
     }
 
-    @RequestMapping("/hostList")
+    @GetMapping("/hostList")
     @ResponseBody
-    public Result<List<String>> hostList() {
-        Application application = eurekaClient.getApplication(APPLICATION_NAME);
-        List<InstanceInfo> instances = Optional.ofNullable(application)
-                .map(Application::getInstances)
-                .orElse(Collections.emptyList());
-        List<String> hostNames = instances.stream().map(InstanceInfo::getHostName).collect(toList());
-        return Result.success(hostNames);
+    public Mono<Result<List<String>>> hostList() {
+        return Mono.justOrEmpty(eurekaClient.getApplication(APPLICATION_NAME))
+                .flatMapIterable(Application::getInstances)
+                .map(InstanceInfo::getHostName)
+                .collect(toList())
+                .defaultIfEmpty(Collections.emptyList())
+                .map(Result::success);
     }
 
-    @RequestMapping("/hotfix")
+    @PostMapping("/hotfix")
     @ResponseBody
-    public Result<HotfixResult> hotfix(@RequestParam("file") MultipartFile file,
+    public Mono<Result<HotfixResult>> hotfix(@RequestParam("file") MultipartFile file,
             @RequestParam("targetPid") String targetPid,
-            @RequestParam(value = "proxyServer", required = false) String proxyServer) throws Exception {
-        if (proxyServer != null && !proxyServer.isEmpty()) {
-            logger.info("Redirect hotfix request {} to {}", file, proxyServer);
-            RequestBody requestBody = RequestBody.create(MediaType.parse("multipart/form-data"),
-                    file.getBytes());
-            MultipartBody.Part classFile = MultipartBody.Part.createFormData("file",
-                    file.getName(), requestBody);
-            RequestBody pidBody = RequestBody.create(MediaType.parse("multipart/form-data"), targetPid);
-            return getProxyClient(proxyServer).reloadClass(classFile, pidBody).execute().body();
-        }
-        String targetClass = hotfixService.hotfix(file, targetPid);
-        return Result.success(new HotfixResult(targetClass));
-    }
-
-    private AgentProxyClient getProxyClient(String proxyServer) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(proxyServer)
-                .addConverterFactory(JacksonConverterFactory.create())
-                .build();
-        return retrofit.create(AgentProxyClient.class);
+            @RequestParam(value = "proxyServer", required = false) String proxyServer) {
+        return Mono.justOrEmpty(proxyServer)
+                .flatMap(proxy -> agentWebClient.reloadClass(file, targetPid, proxy))
+                .switchIfEmpty(Mono.fromCallable(() ->
+                        Result.success(new HotfixResult(hotfixService.hotfix(file, targetPid)))));
     }
 }
